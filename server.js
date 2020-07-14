218da22bbcf5957de8b5209f537b45c2f88c6068
Ganesh Sanap <ganesh.sanap2326@gmail.com>
2020-07-14 13:47:44 +0530

    Added ?completed filter

--- server.js
+++ server.js
@@ -15,9 +15,18 @@ app.get('/', function (req, res) {
     res.send('TODO API Root');
 });
 
-// GET /todos
+// GET /todos?completed=true
 app.get('/todos', function (req, res) {
-    res.json(todos);
+    var filteredTodos = todos;
+    var queryParams = req.query;
+
+    if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+        filteredTodos = _.where(filteredTodos, { completed: true});
+    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+        filteredTodos = _.where(filteredTodos, { completed: false});
+    }
+
+    res.json(filteredTodos);
 });
 
 // GET /todos/:id

b97ca25d14ac443c1c85a595093ff0ae8d0eeda9
Ganesh Sanap <ganesh.sanap2326@gmail.com>
2020-07-14 12:40:02 +0530

    Add PUT todos

--- server.js
+++ server.js
@@ -70,6 +70,37 @@ app.delete('/todos/:id', function (req, res) {
     }
 });
 
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    if (!matchedTodo) {
+        return res.status(404).send();
+    }
+
+    var body = _.pick(req.body, 'description', 'completed');
+
+    var validAttributes = {};
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        res.status(400).send();
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description.trim();
+    } else if (body.hasOwnProperty('description')) {
+        res.status(400).send();
+    }
+
+    _.extend(matchedTodo, validAttributes);
+
+    res.json(matchedTodo);
+    
+});
+
 app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT + '!');
 });

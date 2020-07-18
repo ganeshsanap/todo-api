b095cab8042ab6bb4b168796b3838932bc5ed9ec
Ganesh Sanap <ganesh.sanap2326@gmail.com>
2020-07-18 14:53:34 +0530

    Add todo model and POST/todos

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 
+var db = require('./db.js');
+ 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -61,15 +63,22 @@ app.get('/todos/:id', function (req, res) {
 app.post('/todos', function (req, res) {   
     var body = _.pick(req.body, 'description', 'completed');
 
-    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-        return res.status(400).send();
-    }
+    db.todo.create(body).then(function(todo){
+        res.json(todo.toJSON());
+    }, function (e) {
+        res.status(400).json(e);
+    });
 
-    body.id = todoNextId++;
-    body.description = body.description.trim();
+    //// Old implementation for array
+    // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    //     return res.status(400).send();
+    // }
 
-    todos.push(body);
-    res.json(body);
+    // body.id = todoNextId++;
+    // body.description = body.description.trim();
+
+    // todos.push(body);
+    // res.json(body);
 });
 
 // DELETE /todos/:id
@@ -116,6 +125,8 @@ app.put('/todos/:id', function (req, res) {
     
 });
 
-app.listen(PORT, function () {
-    console.log('Express listening on port ' + PORT + '!');
+db.sequelize.sync().then(function(){
+    app.listen(PORT, function () {
+        console.log('Express listening on port ' + PORT + '!');
+    });
 });

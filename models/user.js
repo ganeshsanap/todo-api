236b8ed19ba9dff532442f49888c56526f15ee39
Ganesh Sanap <ganesh.sanap2326@gmail.com>
2020-07-25 15:17:36 +0530

    Hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
 module.exports = function (sequelize, DataTypes) {
-    return sequelize.define('user', {
+    var user =  sequelize.define('user', {
         email: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -8,11 +11,25 @@ module.exports = function (sequelize, DataTypes) {
                 isEmail: true
             }
         },
+        salt: {
+            type: DataTypes.STRING
+        },
+        password_hash: {
+            type: DataTypes.STRING
+        },
         password: {
-            type: DataTypes.STRING,
+            type: DataTypes.VIRTUAL,
             allowNull: false,
             validate: {
                 len: [7, 100]
+            },
+            set: function(value) {
+                var salt = bcrypt.genSaltSync(10);
+                var hashedPassword = bcrypt.hashSync(value, salt);
+
+                this.setDataValue('password', value);
+                this.setDataValue('salt', salt);
+                this.setDataValue('password_hash', hashedPassword);
             }
         }
     }, {
@@ -24,4 +41,11 @@ module.exports = function (sequelize, DataTypes) {
             }
         }
     });
+
+    user.prototype.toPublicJSON = function() {
+        var json = this.toJSON();
+        return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+    };
+
+    return user;
 };
